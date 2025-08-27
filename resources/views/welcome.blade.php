31322887fa08e640ddccb961e8192704505d094b
Jehad <jehadalabssi@gmail.com>
2025-08-27 16:52:56 +0300

    init 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <meta name="keywords" content="Chroma Creative Studio, creative & digital solutions, branding and design services, logo design, brand identity, UI/UX design, website development, mobile app development, custom illustrations, photography services, videography, motion graphics, 3D modeling, CGI, digital art, interior design, creative storytelling, visual production, innovative design" />
     <meta name="robots" content="index,follow">
     <link rel="canonical" href="{{ url('/') }}">
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/icons/chroma_logo_icon.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/icons/chroma-logo-icon.png') }}">
     <meta name="theme-color" content="#0d6efd">
 
     <meta property="og:type" content="website">
@@ -16,7 +16,7 @@
     <meta property="og:description" content="Chroma Creative Studio is a multidisciplinary powerhouse in branding, design & digital solutions. From logos and web development to photography, motion graphics & interior design, we fuse creativity with technology to deliver impactful experiences." />
     <meta property="og:url" content="{{ url('https://www.instagram.com/chroma.ccs?igsh=ZzN3Znlqdzd0Y3Ex') }}">
     <meta property="og:site_name" content="Chroma.ccs">
-    <meta property="og:image" content="{{ asset('images/under-construction/CCS-Page-big.jpg') }}">
+    <meta property="og:image" content="{{ asset('images/CCS-Page-big.jpg') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -25,12 +25,12 @@
     <picture>
         <!-- Mobile first source -->
         <source
-            srcset="{{ asset('images/under-construction/CCS-Page-small.jpg') }}"
+            srcset="{{ asset('images/CCS-m.jpg') }}"
             media="(max-width: 767.98px)">
 
         <!-- Fallback / default image -->
         <img
-            src="{{ asset('images/under-construction/CCS-Page-big.jpg') }}"
+            src="{{ asset('images/CCS-l.jpg') }}"
             alt="Under construction"
             loading="eager"
             decoding="async"
